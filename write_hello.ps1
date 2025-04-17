37071df6b1df150e04901cb9d76c56994843020c
pvishnu1 <93652983+pvishnu1@users.noreply.github.com>
2025-04-17 15:32:25 +0530

    Create write_hello.ps1

---
+++ write_hello.ps1
@@ -0,0 +1,7 @@
+# write_hello.ps1
+
+$path = "C:\deploy\hello.txt"
+$content = "Hello from Linux!"
+New-Item -ItemType File -Path $path -Force | Out-Null
+Set-Content -Path $path -Value $content
+Write-Host "File written to $path"
