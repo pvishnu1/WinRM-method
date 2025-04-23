7fede5e2f9d57ff4572d55d35cd53ddf3ddf5bc2
pvishnu1 <93652983+pvishnu1@users.noreply.github.com>
2025-04-23 10:45:40 +0530

    Update write_hello.ps1

--- write_hello.ps1
+++ write_hello.ps1
@@ -1,7 +1,7 @@
 # write_hello.ps1
 
 $path = "C:\deploy\hello.txt"
-$content = "Hello from Linux!"
+$content = "Hello from Github!"
 New-Item -ItemType File -Path $path -Force | Out-Null
 Set-Content -Path $path -Value $content
 Write-Host "File written to $path"
